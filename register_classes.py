a3892493ae4726802b4a89d85f113fe7ed5dcf29
pmiller2022 <75953337+pmiller2022@users.noreply.github.com>
2023-08-10 14:46:00 -0400

    Update register_classes.py

--- register_classes.py
+++ register_classes.py
@@ -3,7 +3,7 @@ from labscript_devices import register_classes
 register_classes(
     'PrawnDO',
     BLACS_tab='naqslab_devices.prawn_do.blacs_tabs.PrawnDOTab',
-    runviewer_parser=None,
+    runviewer_parser='naqslab_devices.prawn_do.runviewer_parsers.PrawnDOParser',
 )
 
 
