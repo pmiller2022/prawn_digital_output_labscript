a1186b194a19a41e979e697837f8b93d613f692f
David Meyer <dihm@users.noreply.github.com>
2023-12-08 08:50:22 -0500

    Initial (untested) refactor to pseudoclock device

--- register_classes.py
+++ register_classes.py
@@ -2,13 +2,13 @@ from labscript_devices import register_classes
 
 register_classes(
     'PrawnDO',
-    BLACS_tab='naqslab_devices.prawn_do.blacs_tabs.PrawnDOTab',
-    runviewer_parser='naqslab_devices.prawn_do.runviewer_parsers.PrawnDOParser',
+    BLACS_tab='naqslab_devices.prawn_digital_output_labscript.blacs_tabs.PrawnDOTab',
+    runviewer_parser='naqslab_devices.prawn_digital_output_labscript.runviewer_parsers.PrawnDOParser',
 )
 
 
 register_classes(
-    'PrawnDOTrig',
-    BLACS_tab='naqslab_devices.prawn_do.blacs_tabs.PrawnDOTab',
+    'PrawnDODevice',
+    BLACS_tab='naqslab_devices.prawn_digital_output_labscript.blacs_tabs.PrawnDOTab',
     runviewer_parser=None,
 )
