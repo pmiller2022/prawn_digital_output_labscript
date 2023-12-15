5bea1039aeebe594b6ebeec90bffef1f6fbdffbe
David Meyer <dihm@users.noreply.github.com>
2023-12-15 17:02:07 -0500

    Update runviewer parser to actually load.

--- register_classes.py
+++ register_classes.py
@@ -5,3 +5,9 @@ register_classes(
     BLACS_tab='naqslab_devices.prawn_digital_output_labscript.blacs_tabs.PrawnDOTab',
     runviewer_parser='naqslab_devices.prawn_digital_output_labscript.runviewer_parsers.PrawnDOParser',
 )
+
+register_classes(
+    '_PrawnDOIntermediateDevice',
+    BLACS_tab='',
+    runviewer_parser='naqslab_devices.prawn_digital_output_labscript.runviewer_parsers._PrawnDOIntermediateParser'
+)
